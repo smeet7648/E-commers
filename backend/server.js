bc1dc1eaa879466cd5ea3070011ed03fc923145b
smeet7648 <167986379+smeet7648@users.noreply.github.com>
2026-06-24 12:06:50 +0530

    Remove node_modules from git

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,7 @@ mongoose
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Backend Running");
-});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
