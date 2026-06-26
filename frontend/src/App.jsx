5c943bbb5f657d9b2e5dbeccc4222bf9717af3e5
smeet7648 <167986379+smeet7648@users.noreply.github.com>
2026-06-26 14:40:42 +0530

    buy now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Admin from "./pages/admin";
 import ProductDetails from "./pages/ProductDetails";
-import OrderHistory from "./pages/";
 import Wishlist from "./pages/Wishlist";
 import Invoice from "./pages/Invoice";
 import Profile from "./pages/Profile";
@@ -21,7 +20,6 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/order-history" element={<OrderHistory />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/invoice/" element={<Invoice />} />
         <Route path="/profile" element={<Profile />} />
